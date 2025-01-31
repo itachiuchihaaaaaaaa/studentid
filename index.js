99de30bab19f39db94ae55d73b975f5e9abfecca
unknown <smanurag1234@gmail.com>
2025-01-31 11:04:06 +0530

    updated data

--- index.js
+++ index.js
@@ -30,3 +30,21 @@ app.get('/',async(req,res)=>{
     }
     
 })
+
+app.delete('/remove/:id',async(req,res)=>{
+    try {
+        await stumodel.findByIdAndDelete(req.params.id)
+        res.send("item deleted")
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.put('/update/:id',async(req,res)=>{
+    try {
+        var data=await stumodel.findByIdAndUpdate(req.params.id,req.body)
+        res.send({message:"updated successfully",data})
+    } catch (error) {
+        res.send(error)
+    }
+})
